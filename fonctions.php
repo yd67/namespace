5dd9305bfd67f87a12a879fc56d7302212fa2102
yd67 <yvens67@hotmail.fr>
2021-01-15 15:13:05 +0100

    checkpassword

---
+++ fonctions.php
@@ -0,0 +1,78 @@
+<?php
+
+    /*
+        - créer un namespace (première lettre du prénom + nom | ex.: dhurtrel)
+        - créer une fonction checkPassword permettant de vérifier la composition d'un mot de passe :
+            - 8 caractères
+            - 1 caractère spécial
+            - 1 minuscule
+            - 1 majuscule
+            - 1 chiffre
+        - afficher un message 
+    */
+    
+namespace ydarius;
+
+$mdp='';
+
+ /*function checkPassword($mdp){
+
+  if(strlen($mdp>=8)){
+
+         if (!ctype_lower($mdp)){
+
+                if( preg_match('/([0-9])/',$mdp)){
+                        
+                    
+                       }
+                 }else{  
+                      echo' votre mot de passe doit comporter au moins un chiffre';  }
+
+        }else {
+            echo'votre mot de passe doit comporter au moins  une majuscule ou un minuscule';
+             }
+  
+     } else {
+ };     echo'le mot de passe doit comporter au moins  8 caracteres'; }
+*/
+
+   function checkPassword($mdp){
+                  if(strlen($mdp )>=8){
+                    if (  preg_match("~[0-9]~", $mdp) ){
+
+                        if (  preg_match("~[A-Z]~", $mdp) ){
+                                 if( preg_match("~[a-z]~", $mdp)){
+                                     if(preg_match("/[^a-zA-Z\d]/",$mdp)){
+                                        echo' votre  mot de passe remplie tout les criteres Bravo ' ;
+
+                                    }else{
+                                        echo'le mot de passe doit contenir un caractere special ';
+                                    }
+                                        
+                                    
+
+                                 }else{
+                                  echo'votre mot de pase ne comporte pas de minuscule';
+                              }
+
+
+                          }else{
+                              echo 'votre mot de passe doit comporter une majuscule';
+                          }
+
+                      }else{
+                          echo'votre mot de passe doit comporter au moins un chiffre';
+                      }
+
+                  }
+              else{
+                  echo'le mot de passe doit comporter au moins  8 caracteres';
+              }
+            
+            
+            }
+
+
+
+ 
+?> 
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+    require_once('fonctions.php');
+    $mdp='d4DfFFHh';
+    ydarius\checkPassword($mdp);
+?>
+
+<!DOCTYPE html>
+
+<html lang="fr">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>PHP - 05 - namespace - eval</title>
+    </head>
+
+    <body>
+        
+        <?php
+            /*
+                - tester un mot de passe
+            */
+            
+            
+        ?>
+
+    </body>
+
+</html>
